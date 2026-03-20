caf7de32ed772162b653b3695181bbfffab5acb4
Akash <akash300383@gmail.com>
2026-03-20 06:03:37 +0530

    done

--- FSD - Designing a Dynamic Frontend with React/React JS Projects/react-routing-example/src/Dashboard.jsx
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/react-routing-example/src/Dashboard.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function Dashboard() {
 
+
+let navigate = useNavigate();
+
+let logout = ()=> {
+    navigate("/");
+}
     return(
         <div>
             <h2>Welcome to Dashboard</h2>
+            <input type="button" value="logout" onClick={logout}/>
         </div>
     )
 }
